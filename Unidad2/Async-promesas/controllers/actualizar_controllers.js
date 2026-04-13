519cc951b57460340266d983018fc87ccc557fc7
dfortun07 <davidfortun05@gmail.com>
2026-04-13 10:28:56 -0400

    clase13

---
+++ Unidad2/Async-promesas/controllers/actualizar_controllers.js
@@ -0,0 +1,43 @@
+import { clientService } from "../service/client-service.js";
+const formulario = document.querySelector("[data-form]");
+
+const obInfo = async () => {
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
+    if(id == null){
+        window.location.href = "/screens/error.html";
+    }
+
+    document.querySelector("[data-nombre]")
+    document.querySelector("[data-email]")
+    try {
+        const cliente = await clientService.cliente(id);
+        if(cliente.nombre && cliente.email){
+            document.querySelector("[data-nombre]").value = cliente.nombre;
+            document.querySelector("[data-email]").value = cliente.email;
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        alert("error al cargar los datos");
+        window.location.href = "/screens/error.html";
+    }   
+};
+
+obInfo();
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
+    const nombre = document.querySelector("[data-nombre]").value;
+    const email = document.querySelector("[data-email]").value;
+    clientService.actualizarCliente(nombre, email, id)
+        .then((respuesta) => {
+            console.log("Cliente actualizado:", respuesta);
+            window.location.href = "/screens/actualizacion_completada.html";
+        })
+        .catch((error) => {
+            console.log("Error al actualizar cliente:", error);
+        });
+});
---
+++ Unidad2/Async-promesas/controllers/client_controller.js
@@ -0,0 +1,48 @@
+import { clientService } from "../service/client-service.js";
+
+const CrearFila = (nombre, email, id) => {
+    const fila = document.createElement('tr');
+    const contenido = `
+        <td class="td" data-td>${nombre}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="table__button-control">
+                <li>
+                    
+                        href="../screens/editar_cliente.html?id=${id}"
+                        class="simple-button simple-button--edit"
+                    >
+                        Editar
+                    </a>
+                </li>
+                <li>
+                    <button class="simple-button simple-button--delete" type="button" id="${id}">
+                        Eliminar
+                    </button>
+                </li>
+            </ul>
+        </td>
+    `;
+    fila.innerHTML = contenido;
+    const btn = fila.querySelector("button");
+    btn.addEventListener("click", () => {
+        const id = btn.id;
+        clientService.eliminarCliente(id)
+            .then(() => {
+                alert("eliminado");
+                window.location.reload();
+            })
+            .catch(err => alert("error al eliminar"));
+    });
+    return fila;        
+}
+
+const table = document.querySelector('[data-table]');
+clientService.listar_clientes()
+    .then((data) => {
+        data.forEach((perfil) => {
+            const nuevaFila = CrearFila(perfil.nombre, perfil.email, perfil.id);
+            table.appendChild(nuevaFila);
+        });
+    })
+    .catch((error) => alert("error"));
